72e3e1f7480c760e74e53e785fe698219aae928e
ABlued <53801395+ABlued@users.noreply.github.com>
2022-01-18 21:37:15 +0900

    feat : 댓글 삭제 기능 추가

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -2,27 +2,25 @@ import { Posts } from './Posts';
 import './App.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import Items from './Items';
 function App() {
-	const queryClient = new QueryClient({
-		defaultOptions: {
-			queries: {
-				refetchOnWindowFocus: false,
-			},
-		},
-	});
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
 
-	return (
-		// provide React Query client to App
-		<QueryClientProvider client={queryClient}>
-			<div className="App">
-				{/* <h1>Blog Posts</h1> */}
-				<Items />
-				{/* <Posts /> */}
-			</div>
-			<ReactQueryDevtools />
-		</QueryClientProvider>
-	);
+  return (
+    // provide React Query client to App
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
