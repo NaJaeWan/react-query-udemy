7a272259099968da0153260c70a6c626387d1f1a
ABlued <53801395+ABlued@users.noreply.github.com>
2022-01-19 10:54:10 +0900

    fix : useEffect 호출조건 수정

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -4,94 +4,121 @@ import useInput from './useInput';
 import { fetchComments, updatePost, deletePost } from './api.jsx';
 
 export function PostDetail({ post }) {
-	const { data, isLoading, isError, error } = useQuery(
-		['comment', post.id],
-		() => fetchComments(post.id)
-	);
-	const userName = useInput();
-	const userComment = useInput();
+  const { data, isLoading, isError, error, refetch } = useQuery(
+    ['comment', post.id],
+    () => fetchComments(post.id),
+    {
+      enabled: false,
+    }
+  );
+  const userName = useInput();
+  const userComment = useInput();
 
-	const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
 
-	const deleteMutation = useMutation((commentId) => deletePost(commentId));
-	const updateMutation = useMutation((userInfo) => {
-		updatePost(userInfo);
-	});
+  const deleteMutation = useMutation((commentInfo) => deletePost(commentInfo));
+  const updateMutation = useMutation((userInfo) => {
+    updatePost(userInfo);
+  });
 
-	useEffect(() => {
-		if (updateMutation.isSuccess || deleteMutation.isSuccess) {
-			queryClient.invalidateQueries('comment');
-		}
-	}, [updateMutation.isSuccess, deleteMutation.isSuccess]);
+  useEffect(() => {
+    refetch();
+  }, []);
 
-	if (isLoading) {
-		return <h3>loading...</h3>;
-	}
-	if (isError) {
-		return (
-			<>
-				<h3>Ooops, something went wrong</h3>
-				<p>{error.toString()}</p>
-			</>
-		);
-	}
+  // useEffect(() => {
+  //   console.log('생성', updateMutation.isSuccess, updateMutation.isLoading);
+  //   console.log('삭제', deleteMutation.isSuccess, deleteMutation.isLoading);
+  //   if (
+  //     (updateMutation.isSuccess && !updateMutation.isLoading) ||
+  //     (deleteMutation.isSuccess && !deleteMutation.isLoading)
+  //   ) {
+  //     console.log('패치');
+  //     // refetch();
 
-	return (
-		<>
-			<div>
-				<ul style={{ listStyle: 'none', padding: '0px' }}>
-					<li>
-						<label>name : </label>
-						<input
-							onChange={userName.onChange}
-							type="text"
-							value={userName.value}
-						></input>
-					</li>
-					<li>
-						<label>comment : </label>
-						<input
-							onChange={userComment.onChange}
-							type="text"
-							value={userComment.value}
-						></input>
-					</li>
-				</ul>
-				<button
-					onClick={() => {
-						updateMutation.mutate({
-							postId: post.id,
-							userName: userName.value,
-							userComment: userComment.value,
-						});
-						userName.clear();
-						userComment.clear();
-					}}
-				>
-					Update comment
-				</button>
-			</div>
+  //     // queryClient.invalidateQueries('comment');
+  //     // updateMutation.reset();
+  //     // deleteMutation.reset();
+  //   }
+  // }, [updateMutation.isSuccess, deleteMutation.isSuccess]);
 
-			<h4>Comments</h4>
-			{data.map((userComment, index) => (
-				<div
-					key={userComment.id}
-					style={{
-						display: 'flex',
-						flexDirection: 'row',
-						justifyContent: 'space-between',
-						alignItems: 'center',
-						width: '300px',
-					}}
-				>
-					<li style={{ listStyle: 'none' }}>
-						{index + 1} {userComment.name}: {userComment.comment}
-					</li>
-					<button onClick={() => deleteMutation.mutate(userComment.id)}>
-						삭제
-					</button>
-				</div>
-			))}
-		</>
-	);
+  if (isLoading) {
+    return <h3>loading...</h3>;
+  }
+  if (isError) {
+    return (
+      <>
+        <h3>Ooops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div>
+        <ul style={{ listStyle: 'none', padding: '0px' }}>
+          <li>
+            <label>name : </label>
+            <input
+              onChange={userName.onChange}
+              type="text"
+              value={userName.value}
+            ></input>
+          </li>
+          <li>
+            <label>comment : </label>
+            <input
+              onChange={userComment.onChange}
+              type="text"
+              value={userComment.value}
+            ></input>
+          </li>
+        </ul>
+        <button
+          onClick={() => {
+            updateMutation.mutate({
+              postId: post.id,
+              userName: userName.value,
+              userComment: userComment.value,
+              refetch: refetch,
+            });
+            userName.clear();
+            userComment.clear();
+            // refetch();
+          }}
+        >
+          댓글 작성
+        </button>
+      </div>
+
+      <h4>Comments</h4>
+      {data &&
+        data.map((userComment, index) => (
+          <div
+            key={userComment.id}
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: '300px',
+            }}
+          >
+            <li style={{ listStyle: 'none' }}>
+              {index + 1} {userComment.name}: {userComment.comment}
+            </li>
+            <button
+              onClick={() =>
+                deleteMutation.mutate({
+                  commentId: userComment.id,
+                  refetch: refetch,
+                })
+              }
+            >
+              삭제
+            </button>
+          </div>
+        ))}
+    </>
+  );
 }
